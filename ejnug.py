2a28be55fa3e46c263e8a485590b40bc9d8f7a9d
Thomas Levine <_@thomaslevine.com>
2014-09-15 05:17:20 +0000

    attachments

--- ejnug.py
+++ ejnug.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from unidecode import unidecode
+from lxml.html.clean import clean_html
 from notmuch import Database, Query
 from bottle import Bottle, request, response, abort, redirect, view, TEMPLATE_PATH
 
@@ -56,20 +58,31 @@ def subhierarchy(message):
 
 @app.get('/:querystr/:num')
 def attachment(querystr, num):
+    n = int(num)
     query = Query(db, querystr)
     if query.count_messages() != 1:
         redirect('/%s/' % querystr)
     else:
         message = next(iter(query.search_messages()))
         parts = message.get_message_parts()
-        i = int(num) - 1
+        i = n - 1
         if i >= len(parts):
             redirect('/%s/' % querystr)
         else:
             part = parts[i]
-            response.content_type = part.get_content_type()
+            content_type = part.get_content_type()
+            response.content_type = content_type
          #  response.charset = part.get_content_charset()
-            return part.get_payload()
+
+            fn = part.get_filename()
+            if fn != None:
+                response.headers['content-disposition'] = 'filename=%s;' % unidecode(fn)
+
+            payload = message.get_part(n)
+            if 'html' in content_type.lower():
+                return clean_html(payload)
+            else:
+                return payload
 
 if __name__ == '__main__':
     app.run(server = 'cherrypy', reloader = True)
