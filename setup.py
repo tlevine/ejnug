9039930e5586094673a09895bd925a3048ea49be
Thomas Levine <_@thomaslevine.com>
2014-09-15 18:08:36 +0000

    reuires

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setup(name='ejnug',
       author_email='_@thomaslevine.com',
       description='Share emails publically',
       url='https://github.com/tlevine/ejnug',
-      install_requires=['lxml','unidecode'],
+      install_requires=[
+          'lxml>=3.3.5',
+          'bottle>=0.12.7',
+          'notmuch>=0.15.2',
+          'Unidecode>=0.04.16',
+      ]
       tests_require = ['nose'],
       scripts = ['ejnug.py'],
       version='0.0.1',
